0e1f3327c0582dc5b70d99cbb60d3b71555cc551
mehmetozk4n6 <81770287+mehmetozk4n6@users.noreply.github.com>
2022-04-19 15:24:35 +0300

    Completed  except for last one

---
+++ src/App.test.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "./App";
+
+describe("Emoji search testleri", () => {
+  let header, emojiListFirst, emojiListLast, inputEmoji;
+
+  beforeEach(() => {
+    render(<App />);
+    header = screen.getByText(/Emoji Search/i);
+    emojiListFirst = screen.getByText(/100/i);
+    emojiListLast = screen.getByText(/Kissing Heart/i);
+    inputEmoji = screen.getByPlaceholderText(/Search Emoji/i);
+  });
+  //should got header
+  it("should got header", () => {
+    expect(header.toBeInTheDocument);
+  });
+  //should got emojiList
+  it("should got emojiListFirstElement", () => {
+    expect(emojiListFirst.toBeInTheDocument);
+  });
+  it("should got emojiListLastElement", () => {
+    expect(emojiListLast.toBeInTheDocument);
+  });
+  //should filter according to text
+  it("should filter according to text", () => {
+    let emojiSearch = "Wink";
+    userEvent.type(inputEmoji, emojiSearch);
+    let searchedElement = screen.getByText(emojiSearch);
+    expect(searchedElement.toBeInTheDocument);
+  });
+  //when clicked , should copy
+  it("should copy", () => {
+    userEvent.click(emojiListFirst);
+    expect(emojiListFirst).toHaveAttribute("data-clipboard-text");
+  });
+});
